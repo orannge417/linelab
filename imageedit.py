875b4a753a4746f06a74ef075ba435fec389c09c
orangewillow <orange.widow417@gmail.com>
2021-07-02 03:47:41 +0900

    image try commit 2

---
+++ imageedit.py
@@ -0,0 +1,32 @@
+from PIL import Image, ImageDraw, ImageFont
+
+def imageedit(image=None):
+    if image is None:
+        return 'Image is not being processed successfully'
+    
+    im = Image.open(image)
+    draw = ImageDraw.Draw(im)
+
+    size = 800
+    if im.width > size:
+        proportion = size / im.width
+    im = im.resize((int(im.width * proportion), int(im.height * proportion)))
+
+    text = "apple!"
+
+    # フォントの読み込み
+    font = ImageFont.truetype("./fonts/Harlow Solid Regular.ttf", 60)
+
+    # テキストの周りの5pxずつに余白を作る
+    x = 10
+    y = 10
+    margin = 5
+    text_width = draw.textsize(text, font=font)[0] + margin
+    text_height = draw.textsize(text, font=font)[1] + margin
+    draw.rectangle(
+    (x - margin, y - margin, x + text_width, y + text_height), fill=(255, 255, 255)
+    )
+    # テキスト描画
+    draw.text((x, y), text, fill=(0, 0, 0), font=font)
+
+    return im
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-#LINE BOT のサンプルからいじる
+# LINE BOT のサンプルからいじる
+# https://qiita.com/sayama0402/items/e2c9e65786259dc55e11
+# https://qiita.com/tamago324/items/4df361fd6ac5b51a8a07
 
+from io import BytesIO
+
+from linebot.models.send_messages import ImageSendMessage
+from linebot2.imageedit import imageedit
 from flask import Flask, request, abort
 
 from linebot import (
@@ -50,6 +56,23 @@ def handle_message(event):
 
 @handler.add(MessageEvent, message=ImageMessage)
 def handleimage(event):
+    # メッセージのIDを取得
+    message_id = event.message.id
+    
+    # message_idから画像のバイナリーデータを取得
+    message_content = line_bot_api.get_message_content(message_id)
+
+    image = BytesIO(message_content.content)
+
+    try:
+        result = imageedit(image)
+
+        if isinstance(result, str):
+            TextSendMessage(text=result)
+            
+        elif isinstance(result, image):
+            ImageSendMessage(result)
+
     line_bot_api.reply_message(
         event.reply_token,
         TextMessage(text='This is image.'))
