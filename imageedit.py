7d34971807e5f29aa5235baa63ec498aaec6b5e6
orangewillow <orange.widow417@gmail.com>
2021-07-02 04:36:04 +0900

    image try commit

--- imageedit.py
+++ imageedit.py
@@ -1,41 +1,38 @@
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw
 import os 
 
 def imageedit(image):
+    try:
+        os.remove("data/image.png")
+    except FileNotFoundError:
+        print("hoge")
+
+    if image is None:
+        return 0
     
-    # try:
-    #     os.remove("data/image.png")
-    # except FileNotFoundError:
-    #     print("hoge")
+    im = Image.open(image)
+    draw = ImageDraw.Draw(im)
 
-    # if image is None:
-    #     return 0
-    
-    # im = Image.open(image)
-    # draw = ImageDraw.Draw(im)
+    size = 800
+    if im.width > size:
+        proportion = size / im.width
+    im = im.resize((int(im.width * proportion), int(im.height * proportion)))
 
-    # size = 800
-    # if im.width > size:
-    #     proportion = size / im.width
-    # im = im.resize((int(im.width * proportion), int(im.height * proportion)))
+    text = "apple!"
 
-    # text = "apple!"
 
-    # # フォントの読み込み
-    # font = ImageFont.truetype("./fonts/Harlow Solid Regular.ttf", 60)
+    # テキストの周りの5pxずつに余白を作る
+    x = 10
+    y = 10
+    margin = 5
+    text_width = draw.textsize(text)[0] + margin
+    text_height = draw.textsize(text)[1] + margin
+    draw.rectangle(
+    (x - margin, y - margin, x + text_width, y + text_height), fill=(255, 255, 255)
+    )
+    # テキスト描画
+    draw.text((x, y), text, fill=(0, 0, 0))
 
-    # # テキストの周りの5pxずつに余白を作る
-    # x = 10
-    # y = 10
-    # margin = 5
-    # text_width = draw.textsize(text, font=font)[0] + margin
-    # text_height = draw.textsize(text, font=font)[1] + margin
-    # draw.rectangle(
-    # (x - margin, y - margin, x + text_width, y + text_height), fill=(255, 255, 255)
-    # )
-    # # テキスト描画
-    # draw.text((x, y), text, fill=(0, 0, 0), font=font)
-
-    # im.save("//data//image.png")
+    im.save("//data//image.png")
 
     return 'a'
--- main.py
+++ main.py
@@ -66,11 +66,11 @@ def handleimage(event):
     imageedit(image)
 
     image_message = 'a'
-    # main_image_path= f"data/image.png"
+    main_image_path= f"data/image.png"
 
-    # image_message = ImageSendMessage(
-    #     main_content_url = f"https://linebotforapp2.herokuapp.com/{main_image_path}"
-    # )
+    image_message = ImageSendMessage(
+        main_content_url = f"https://linebotforapp2.herokuapp.com/{main_image_path}"
+    )
 
     line_bot_api.reply_message(event.reply_token, image_message)
         # event.reply_token,
