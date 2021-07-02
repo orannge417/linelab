742ecca817a903343ee467fb316232f47d644a2b
orangewillow <orange.widow417@gmail.com>
2021-07-02 16:19:40 +0900

    initialize

--- main.py
+++ main.py
@@ -58,38 +58,38 @@ def handle_message(event):
 @handler.add(MessageEvent, message=ImageMessage)
 def handleimage(event):
     # メッセージのIDを取得
-    message_id = event.message.id
+    # message_id = event.message.id
     
 
-    src_image_path = Path("data/images/{}.jpg".format(message_id)).absolute()
-    main_image_path = "data/images/{}_main.jpg".format(message_id)
-    preview_image_path = "data/images/{}_preview.jpg".format(message_id)
+    # src_image_path = Path("data/images/{}.jpg".format(message_id)).absolute()
+    # main_image_path = "data/images/{}_main.jpg".format(message_id)
+    # preview_image_path = "data/images/{}_preview.jpg".format(message_id)
 
-    # 画像を保存
-    save_image(message_id, src_image_path)
+    # # 画像を保存
+    # save_image(message_id, src_image_path)
 
-    imageedit(src=src_image_path, desc=Path(main_image_path).absolute())
-
-
-
-    image_message = ImageSendMessage(
-        main_content_url = f"https://linebotforlab2.herokuapp.com/{main_image_path}",
-        preview_image_url = f"https://linebotforlab2.herokuapp.com/{preview_image_path}",
-    )
-
-    app.logger.info(f"https://linebotforlab2.herokuapp.com/{main_image_path}")
-
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=str(message_id)))
+    # imageedit(src=src_image_path, desc=Path(main_image_path).absolute())
 
 
-    line_bot_api.reply_message(event.reply_token, image_message)
 
-def save_image(message_id: str, save_path: str) -> None:
-    """保存"""
-    message_content = line_bot_api.get_message_content(message_id)
-    with open(save_path, "wb") as f:
-        for chunk in message_content.iter_content():
-            f.write(chunk)
+    # image_message = ImageSendMessage(
+    #     main_content_url = f"https://linebotforlab2.herokuapp.com/{main_image_path}",
+    #     preview_image_url = f"https://linebotforlab2.herokuapp.com/{preview_image_path}",
+    # )
+
+    # app.logger.info(f"https://linebotforlab2.herokuapp.com/{main_image_path}")
+
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text='This is image'))
+
+
+    # line_bot_api.reply_message(event.reply_token, image_message)
+
+# def save_image(message_id: str, save_path: str) -> None:
+#     """保存"""
+#     message_content = line_bot_api.get_message_content(message_id)
+#     with open(save_path, "wb") as f:
+#         for chunk in message_content.iter_content():
+#             f.write(chunk)
 
 if __name__ == "__main__":
 #    app.run()
